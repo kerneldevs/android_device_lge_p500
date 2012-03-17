263ac13584c9d6703ede79c06c6c17ee3bfbc4f6
Guilherme Maganha Moreira <guilhermerx7@gmail.com>
2012-03-17 00:45:40 -0300

    changes so aokp can compile

--- aokp.mk
+++ aokp.mk
@@ -1,16 +1,16 @@
 # Include GSM stuff
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/aokp/configs/gsm.mk)
 
 # Inherit device configuration for p500.
 $(call inherit-product, device/lge/p500/p500.mk)
 
 # Inherit some common cyanogenmod stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+#$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Include FM-Radio stuff
 #$(call inherit-product, vendor/cyanogen/products/bcm_fm_radio.mk)
 
-PRODUCT_NAME := cm_p500
+PRODUCT_NAME := aokp_p500
 PRODUCT_BRAND := lge
 PRODUCT_DEVICE := p500
 PRODUCT_MODEL := LG-P500
@@ -20,4 +20,4 @@ PRODUCT_MANUFACTURER := LGE
 # Release name and versioning
 PRODUCT_RELEASE_NAME := OptimusOne
 PRODUCT_VERSION_DEVICE_SPECIFIC :=
--include vendor/cyanogen/products/common_versions.mk
+-include vendor/aokp/configs/common_versions.mk
